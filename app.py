58a98688206415e41e97eec20c262ea4c714547a
Gurpreet Chugh <chugh.gurpreet@gmail.com>
2023-06-13 23:36:39 -0400

    Commit

--- app.py
+++ app.py
@@ -79,7 +79,7 @@ def wages_points(league):
     avg_points = []
     for row in response_wages_pts:
         squad_name = row.squad
-        wage = row.avgofannual_wages
+        wage = row.avgofannual_wages/1000000
         point = row.avg_pts
         squad_names.append(squad_name)
         avg_wages.append(wage)
@@ -151,63 +151,6 @@ def goals(league):
                      'values': values}
 
     return jsonify(team_player_goals)
-
- 
- #def home():
-#     stadiums = [
-#         {
-#             'name': 'Wembley Stadium',
-#             'location': 'London, England',
-#             'capacity': '90,000',
-#             'image': 'wembley.jpg',
-#             'country': 'England'
-#         },
-#         {
-#             'name': 'Emirates Stadium',
-#             'location': 'London, England',
-#             'capacity': '60,704',
-#             'image': 'emirates.jpg',
-#             'country': 'England'
-#         },
-#         {
-#             'name': 'Camp Nou',
-#             'location': 'Barcelona, Spain',
-#             'capacity': '99,354',
-#             'image': 'camp_nou.jpg',
-#             'country': 'Spain'
-#         },
-#         {
-#             'name': 'Santiago Bernabeu',
-#             'location': 'Madrid, Spain',
-#             'capacity': '81,044',
-#             'image': 'bernabeu.jpg',
-#             'country': 'Spain'
-#         },
-#         {
-#             'name': 'Allianz Arena',
-#             'location': 'Munich, Germany',
-#             'capacity': '75,000',
-#             'image': 'allianz_arena.jpg',
-#             'country': 'Germany'
-#         },
-#         {
-#             'name': 'San Siro',
-#             'location': 'Milan, Italy',
-#             'capacity': '75,923',
-#             'image': 'san_siro.jpg',
-#             'country': 'Italy'
-#         },
-#         {
-#             'name': 'Parc des Princes',
-#             'location': 'Paris, France',
-#             'capacity': '47,929',
-#             'image': 'parc_des_princes.jpg',
-#             'country': 'France'
-#         },
-#         # Add more stadiums here
-#     ]
-
-#     return render_template('index.html', stadiums=stadiums)
 
 if __name__ == '__main__':
     app.run(debug=True)
--- static/js/app.js
+++ static/js/app.js
@@ -1,110 +1,110 @@
 // code to dynamically create dropdown options
-leagues = ['EPL', 'Bundesliga', 'LaLiga', 'Ligue1', 'SerieA']
+var leagueNames = ['EPL', 'Bundesliga', 'LaLiga', 'Ligue1', 'SerieA']
 d3.select('#selDataset')   // select 'select' element from html with id = 'selDataset
     .selectAll('option')
-    .data(leagues)
+    .data(leagueNames)
     .enter()
     .append('option')
     .attr('value', value => value)
     .text(value => value)
 
-// Initial code to create a leaflet map using EPL data
-// Store our API endpoint as queryUrl.
-let queryUrl = fetch('stadiums_final.geojson')
-.then(response => response.json())
-.then(data => {
-  // Process the GeoJSON data
-  console.log(data);
-  // Access specific properties or features
-  // console.log(features);
-  // console.log(feature[0].properties);
-  // ...
-})
-.catch(error => {
-  console.error('Error:', error);
-});
+// // Initial code to create a leaflet map using EPL data
+// // Store our API endpoint as queryUrl.
+// let queryUrl = fetch('stadiums_final.geojson')
+// .then(response => response.json())
+// .then(data => {
+//   // Process the GeoJSON data
+//   console.log(data);
+//   // Access specific properties or features
+//   // console.log(features);
+//   // console.log(feature[0].properties);
+//   // ...
+// })
+// .catch(error => {
+//   console.error('Error:', error);
+// });
 
-// let url = '/api'
-let Coords = [52.3555, 1.1743];
-let mapZoomLevel = 5;
+// // let url = '/api'
+// let Coords = [52.3555, 1.1743];
+// let mapZoomLevel = 5;
 
 
-// Create the createMap function.
-function createMap (europeanStadiums) {
+// // Create the createMap function.
+// function createMap (europeanStadiums) {
 
-  // Create the tile layer that will be the background of our map.
-  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-  });
+//   // Create the tile layer that will be the background of our map.
+//   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+//   });
 
-  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-  });
+//   let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+//   });
 
-  // Create a baseMaps object to hold the lightmap layer.
-  let baseMaps = {
-    "Street Map" : street,
-    "Topographic" : topo
-  }
+//   // Create a baseMaps object to hold the lightmap layer.
+//   let baseMaps = {
+//     "Street Map" : street,
+//     "Topographic" : topo
+//   }
   
 
-  // Create an overlayMaps object to hold the Stadiums layer.
-  let overlayMaps = {
-    "Footbal Stadiums" : europeanStadiums
- }
+//   // Create an overlayMaps object to hold the Stadiums layer.
+//   let overlayMaps = {
+//     "Footbal Stadiums" : europeanStadiums
+//  }
 
-  // Create the map object with options.
-  let myMap = L.map("map", {
-    center: Coords,
-    zoom: mapZoomLevel,
-    layers: [street, europeanStadiums]
-  })
+//   // Create the map object with options.
+//   let myMap = L.map("map", {
+//     center: Coords,
+//     zoom: mapZoomLevel,
+//     layers: [street, europeanStadiums]
+//   })
 
-  // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-  L.control.layers(baseMaps, overlayMaps, {
-    collapsed: false
-  }).addTo(myMap)
-}
+//   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+//   L.control.layers(baseMaps, overlayMaps, {
+//     collapsed: false
+//   }).addTo(myMap)
+// }
 
-// Create the createMarkers function.
-function createMarkers(response) {
+// // Create the createMarkers function.
+// function createMarkers(response) {
 
-  // Pull the "stadiums" property from response.data and filter for England
-  let stadiums = response.features.filter(function(feature) {
-    return feature.properties.Country === "England";
-  })
+//   // Pull the "stadiums" property from response.data and filter for England
+//   let stadiums = response.features.filter(function(feature) {
+//     return feature.properties.Country === "England";
+//   })
 
-  // Initialize an array to hold the Stadium markers.
-  let stadiumMarkers = []
-  // console.log(stadiums)
+//   // Initialize an array to hold the Stadium markers.
+//   let stadiumMarkers = []
+//   // console.log(stadiums)
 
-  // Loop through the Stadiums array.
-  for (let i = 0; i < stadiums.length; i++) {
-    let stadium = stadiums[i]
-    let stadCoords = [stadium.geometry.coordinates[1], stadium.geometry.coordinates[0]]
+//   // Loop through the Stadiums array.
+//   for (let i = 0; i < stadiums.length; i++) {
+//     let stadium = stadiums[i]
+//     let stadCoords = [stadium.geometry.coordinates[1], stadium.geometry.coordinates[0]]
 
-    var soccerIcon = L.icon({
-      iconUrl: 'static/football_marker.png',
-      iconSize: [50, 50], // Adjust the size of the icon as needed
-    });
+//     var soccerIcon = L.icon({
+//       iconUrl: 'static/football_marker.png',
+//       iconSize: [50, 50], // Adjust the size of the icon as needed
+//     });
     
     
-    // For each Stadium, create a marker, and bind a popup with the Stadiums's name.
-    let stadiumMarker = L.marker((stadCoords),{icon: soccerIcon}).bindPopup(`<h1> ${stadium.properties.Stadium}</h1><hr><h2>Capcity: ${stadium.properties.Cap}</h2><hr><h3>Fact: ${stadium.properties.Trivia}</h3>`)
+//     // For each Stadium, create a marker, and bind a popup with the Stadiums's name.
+//     let stadiumMarker = L.marker((stadCoords),{icon: soccerIcon}).bindPopup(`<h1> ${stadium.properties.Stadium}</h1><hr><h2>Capcity: ${stadium.properties.Cap}</h2><hr><h3>Fact: ${stadium.properties.Trivia}</h3>`)
     
-    // Add the marker to the stadiumMarkers array.
-    // console.log(stadCoords)
-    stadiumMarkers.push(stadiumMarker)
-  }
-  // Create a layer group that's made from the stadium markers array, and pass it to the createMap function.
-  createMap(L.layerGroup(stadiumMarkers ))
-}
+//     // Add the marker to the stadiumMarkers array.
+//     // console.log(stadCoords)
+//     stadiumMarkers.push(stadiumMarker)
+//   }
+//   // Create a layer group that's made from the stadium markers array, and pass it to the createMap function.
+//   createMap(L.layerGroup(stadiumMarkers ))
+// }
 
 
-// Perform an API call to the Stadmium API to get the station information. Call createMarkers when it completes.
-d3.json('stadiums_final.geojson').then(createMarkers);
+// // Perform an API call to the Stadmium API to get the station information. Call createMarkers when it completes.
+// d3.json('stadiums_final.geojson').then(createMarkers);
 
-// intial code to create sunburst chart using EPL data
+// // intial code to create sunburst chart using EPL data
 
 
 
@@ -112,141 +112,121 @@ d3.json('stadiums_final.geojson').then(createMarkers);
 
 d3.json("http://127.0.0.1:5000/api/wages/points/EPL").then(data => {
     console.log(data)
+    let leagueWagePoints = data
+
+    let xPoints = leagueWagePoints.points.slice(0,6); // avg points for all the seasons for top 6 teams
+    let xWage = leagueWagePoints.avg_wage.slice(0,6); // avg wage for all the seasons for top 6 teams
+    let yLabels = leagueWagePoints.squad_name.slice(0,6); // Top 6 team labels
+
+    let traceBar = {
+        x: xPoints,
+        y: yLabels,
+        xaxis: 'x1',
+        yaxis: 'y1',
+        type: 'bar',
+        marker: {
+          color: 'rgba(204,0,0,0.6)',  // color based on league
+          line: {
+                color: 'rgba(204,0,0,1.0)',
+                width: 1
+                }
+        },
+        name: 'Points',
+        orientation: 'h'
+      };
+
+    let traceLine = {
+        x: xWage,
+        y: yLabels,
+        xaxis: 'x2',
+        yaxis: 'y1',
+        mode: 'lines+markers',
+        line: {
+          color: 'rgba(204,0,0,0.6)' //  color based on league
+        },
+        name: 'Wage in USD Million'
+      };
+      var data = [traceBar, traceLine];
+  
+    let layoutBarLine = {
+    title: 'Average Points and Wage for top 6 teams in 5 seasons(2017-2022)',
+        xaxis1: {
+          range: [Math.min(...xPoints)-10, Math.max(...xPoints)+10],
+          domain: [0, 0.5],
+          zeroline: false,
+          showline: false,
+          showticklabels: true,
+          showgrid: true
+        },
+        xaxis2: {
+          range: [Math.min(...xWage)-20, Math.max(...xWage)+20],
+          domain: [0.5, 1],
+          zeroline: false,
+          showline: false,
+          showticklabels: true,
+          showgrid: true,
+          side: 'top',
+          dtick: 20
+        },
+        legend: {
+          x: 0.029,
+          y: 1.238,
+          font: {
+            size: 10
+          }
+        },
+        margin: {
+          l: 100,
+          r: 20,
+          t: 200,
+          b: 70
+        },
+        width: 400,
+        height: 400,
+        // paper_bgcolor: 'rgb(248,248,255)',
+        // plot_bgcolor: 'rgb(248,248,255)',
+        annotations: []
+  };
+
+  for ( let i = 0 ; i < xPoints.length ; i++ ) {
+    let result1 = {
+      xref: 'x1',
+      yref: 'y1',
+      x: xPoints[i]+2.3,
+      y: yLabels[i],
+      text: xPoints[i],
+      font: {
+        family: 'Arial',
+        size: 12,
+        color: 'rgb(128,128,128)'   
+      },
+       showarrow: false,
+    };
+    var result2 = {
+      xref: 'x2',
+      yref: 'y1',
+      x: xWage[i] + 5,
+      y: yLabels[i],
+      text: xWage[i] + 'M',
+      font: {
+        family: 'Arial',
+        size: 12,
+        color: 'rgb(128,128,128)' 
+      },
+       showarrow: false
+    };
+    layout.annotations.push(result1, result2);
+  }
+  
+  Plotly.newPlot('barline', data, layout);
 }
 )
-
-// var xPoints = [  ]; // avg points for all the seasons for top 6 teams
-  
-// var xWage = [ ]; // avg wage for all the seasons for top 6 teams
-  
-// var yLabels = []; // Top 6 team labels
-  
-// var traceBar = {
-//     x: xPoints,
-//     y: yLabels,
-//     xaxis: 'x1',
-//     yaxis: 'y1',
-//     type: 'bar',
-//     marker: {
-//       color: 'rgba(50,171,96,0.6)',  // color based on league
-//     //   line: {
-//     //     color: 'rgba(50,171,96,1.0)',
-//     //     width: 1
-//     //   }
-//     },
-//     name: 'Points',
-//     orientation: 'h'
-//   };
-  
-//   var traceLine = {
-//     x: xWage,
-//     y: yLabels,
-//     xaxis: 'x2',
-//     yaxis: 'y1',
-//     mode: 'lines+markers',
-//     line: {
-//       color: 'rgb(128,0,128)' //  color based on league
-//     },
-//     name: 'Wage'
-//   };
-  
-//   var data = [traceBar, traceLine];
-  
-//   var layout = {
-//     title: 'Average Points and Wage for top 6 teams in 5 seasons(2017-2022)',
-//     xaxis1: {
-//       range: [0, 100],
-//       domain: [0, 0.5],
-//       zeroline: false,
-//       showline: false,
-//       showticklabels: true,
-//       showgrid: true
-//     },
-//     xaxis2: {
-//       range: [100000000, 210000000],
-//       domain: [0.5, 1],
-//       zeroline: false,
-//       showline: false,
-//       showticklabels: true,
-//       showgrid: true,
-//       side: 'top',
-//       dtick: 25000000
-//     },
-//     legend: {
-//       x: 0.029,
-//       y: 1.238,
-//       font: {
-//         size: 10
-//       }
-//     },
-//     margin: {
-//       l: 100,
-//       r: 20,
-//       t: 200,
-//       b: 70
-//     },
-//     width: 600,
-//     height: 600,
-//     paper_bgcolor: 'rgb(248,248,255)',
-//     plot_bgcolor: 'rgb(248,248,255)',
-//     annotations: [
-//     //   {
-//     //     xref: 'paper',
-//     //     yref: 'paper',
-//     //     x: -0.2,
-//     //     y: -0.109,
-//     //     text: 'OECD ' + '(2015), Household savings (indicator), ' + 'Household net worth (indicator). doi: ' + '10.1787/cfc6f499-en (Accessed on 05 June 2015)',
-//     //     showarrow: false,
-//     //     font:{
-//     //       family: 'Arial',
-//     //       size: 10,
-//     //       color: 'rgb(150,150,150)'
-//     //     }
-//     //   }
-//     ]
-//   };
-  
-//   for ( var i = 0 ; i < xPoints.length ; i++ ) {
-//     var result = {
-//       xref: 'x1',
-//       yref: 'y1',
-//       x: xPoints[i]+2.3,
-//       y: yLabels[i],
-//       text: xPoints[i],
-//       font: {
-//         family: 'Arial',
-//         size: 12,
-//         // color: 'rgb(50, 171, 96)' // color based on league
-//       },
-//        showarrow: false,
-//     };
-//     var result2 = {
-//       xref: 'x2',
-//       yref: 'y1',
-//       x: xWage[i] + 10000000,
-//       y: yLabels[i],
-//       text: xWage[i] + ' M',
-//       font: {
-//         family: 'Arial',
-//         size: 12,
-//         // color: 'rgb(128, 0, 128)' //  color based on league
-//       },
-//        showarrow: false
-//     };
-//     layout.annotations.push(result, result2);
-//   }
-  
-//   Plotly.newPlot('bar', data, layout);
-
-
-
-
 //  with above page will load with EPL data by default
 
 //  then we  write the code to handle event change when we select the league from drop down
 
 
-// function update_leaflet(league) {
+// function updateLeaflet(league) {
 //     d3.json(league API).then(data => {
 //         let response = data;
 //         // update leaflet code
@@ -257,7 +237,7 @@ d3.json("http://127.0.0.1:5000/api/wages/points/EPL").then(data => {
 // }
 
 
-// function update_sunBurst(league) {
+// function updateSunburst(league) {
 //     d3.json(league API).then(data => {
 //         let response = data;
 
@@ -265,36 +245,64 @@ d3.json("http://127.0.0.1:5000/api/wages/points/EPL").then(data => {
 //     })
 // }
 
-// function update_bar(league) {
-//     d3.json(league API).then(data => {
-//         let response = data;
+function updateBarLine(league) {
+    let base_url = "http://127.0.0.1:5000/api/wages/points/"
 
-//         // update bar code
-//         let xPoints = []  // based on league selected
-//         let xWage = [] // based on new league selected
-//         let yLabels = [] // based on new league selected
+    d3.json(`${base_url}${league}`).then(data => {
+        let updatedWagePoints = data;
+        let xPoints = updatedWagePoints.points.slice(0,6);  
+        let xWage = updatedWagePoints.avg_wage.slice(0,6);
+        let yLabels = leagueWagePoints.squad_name.slice(0,6);
 
-//         let updateBar = {
-//             x: [[xPoints], [xWage]],
-//             y: [yLabels],
-//             marker: {
-//                 color: ['color 1', 'color 2']           
-//             }
-//         }
+        if (league = 'EPL') {
+          colorMarker = 'rgba(204,0,0,0.6)'
+          colorMarkerLine = 'rgba(204,0,0,1.0)'
+        }  else if (league = 'Bundesliga') {
+          colorMarker= 'rgba(255,204,0,0.6)'
+          colorMarkerLine = 'rgba(255,204,0,1.0)' 
+        } else if (league = 'SerieA') {
+          colorMarker = 'rgba(0,146,70,0.6)'
+          colorMarkerLine = 'rgba(0,146,70,1.0)' 
+        } else if (league = 'LaLiga') {
+          colorMarker = 'rgba(170,0,0,0.6)'
+          colorMarkerLine = 'rgba(170,0,0,1.0)' 
+        } else if (league = 'Ligue1') {
+          colorMarker = 'rgba(0,35,150,0.6)'
+          colorMarkerLine = 'rgba(0,35,150,1.0)'
+        } 
 
-//         Plotly.restyle('bar', updateBar, [0, 1])
-//     })
-// }
+
+        let updatedData = [
+          {
+            x: [xPoints],
+            y: [yLabels],
+            marker: {
+                color: colorMarker,
+                line: {color: colorMarkerLine}          
+            }
+          },
+          {
+            x: [xWage],
+            y: [yLabels],
+            line: {color: colorMarker}
+          }
+
+        ]
+
+        Plotly.restyle('bar', updatedData, [0, 1])
+    })
+}
 
 
 
-// d3.select('select').on('change', function() {
+d3.select('select').on('change', function() {
+    let league_selected = this.value
 
-//     update_leaflet(this.value)
+//     updateLeaflet(league_selected)
 
-//     update_sunburts(this.value)
+//     updateSunburst(league_selected)
 
-//     update_bar(this.value)
+    updateBarLine(league_selected)
 
-// })
+})
 
