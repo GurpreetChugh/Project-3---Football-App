43b8ece1ab3337ee45b0438ae7544722090772d0
Gavin-Toole <121550462+Gavin-Toole@users.noreply.github.com>
2023-06-15 06:34:58 -0230

    got leaflet working

--- static/js/app.js
+++ static/js/app.js
@@ -24,85 +24,85 @@
 // let Coords = [52.3555, 1.1743];
 // let mapZoomLevel = 5;
 
-// Create the createMap function.
-// function createMap (europeanStadiums) {
 
-//   // Create the tile layer that will be the background of our map.
-//   let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
-//   attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
-//   });
+function createMap (europeanStadiums) {
 
-//   let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
-//     attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
-//   });
+  // Create the tile layer that will be the background of our map.
+  let street = L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+  attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+  });
 
-//   // Create a baseMaps object to hold the lightmap layer.
-//   let baseMaps = {
-//     "Street Map" : street,
-//     "Topographic" : topo
-//   }
+  let topo = L.tileLayer('https://{s}.tile.opentopomap.org/{z}/{x}/{y}.png', {
+    attribution: 'Map data: &copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors, <a href="http://viewfinderpanoramas.org">SRTM</a> | Map style: &copy; <a href="https://opentopomap.org">OpenTopoMap</a> (<a href="https://creativecommons.org/licenses/by-sa/3.0/">CC-BY-SA</a>)'
+  });
+
+  // Create a baseMaps object to hold the lightmap layer.
+  let baseMaps = {
+    "Street Map" : street,
+    "Topographic" : topo
+  }
   
 
-//   // Create an overlayMaps object to hold the Stadiums layer.
-//   let overlayMaps = {
-//     "Footbal Stadiums" : europeanStadiums
-//  }
+  // Create an overlayMaps object to hold the Stadiums layer.
+  let overlayMaps = {
+    "Footbal Stadiums" : europeanStadiums
+ }
 
-//   // Create the map object with options.
-//   let Coords = [52.3555, 1.1743];
-//   let mapZoomLevel = 5;
-//   let myMap = L.map("map-id", {
-//     center: Coords,
-//     zoom: mapZoomLevel,
-//     layers: [street, europeanStadiums]
-//   })
+  // Create the map object with options.
+  let Coords = [53.3555, 1.1743];
+  let mapZoomLevel = 6;
+  let myMap = L.map("map-id", {
+    center: Coords,
+    zoom: mapZoomLevel,
+    layers: [street, europeanStadiums]
+  })
 
-//   // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
-//   L.control.layers(baseMaps, overlayMaps, {
-//     collapsed: false
-//   }).addTo(myMap)
-// }
+  // Create a layer control, and pass it baseMaps and overlayMaps. Add the layer control to the map.
+  L.control.layers(baseMaps, overlayMaps, {
+    collapsed: false
+  }).addTo(myMap)
+}
 
-// // Create the createMarkers function.
-// function createMarkers(response) {
-//   console.log(response)
-//   // Pull the "stadiums" property from response.data and filter for England
-//   let stadiums = response.features
+// Create the createMarkers function.
+function createMarkers(response) {
+  console.log(response)
+  // Pull the "stadiums" property from response.data and filter for England
+  let stadiums = response.features
 
-//   // Initialize an array to hold the Stadium markers.
-//   let stadiumMarkers = []
-//   console.log(stadiums)
+  // Initialize an array to hold the Stadium markers.
+  let stadiumMarkers = []
+  console.log(stadiums)
 
-//   // Loop through the Stadiums array.
-//   for (let i = 0; i < stadiums.length; i++) {
-//     let stadium = stadiums[i]
-//     console.log(stadium)
-//     let stadCoords = [stadium.geometry.coordinates[0], stadium.geometry.coordinates[1]]
-//     console.log(stadCoords)
+  // Loop through the Stadiums array.
+  for (let i = 0; i < stadiums.length; i++) {
+    let stadium = stadiums[i]
+    console.log(stadium)
+    let stadCoords = [stadium.geometry.coordinates[0], stadium.geometry.coordinates[1]]
+    console.log(stadCoords)
     
-//     var soccerIcon = L.icon({
-//       iconUrl: '../static/football_marker.png',
-//       iconSize: [50, 50], // Adjust the size of the icon as needed
-//     });
+    var soccerIcon = L.icon({
+      iconUrl: '../static/football_marker.png',
+      iconSize: [50, 50], // Adjust the size of the icon as needed
+    });
     
-//     // For each Stadium, create a marker, and bind a popup with the Stadiums's name.
-//     let stadiumMarker = L.marker((stadCoords),{icon: soccerIcon}).bindPopup(`<h1> ${stadium.properties.stadium}</h1><hr><h2>Capcity: ${stadium.properties.cap}</h2><hr><h3>Fact: ${stadium.properties.trivia}</h3>`)
+    // For each Stadium, create a marker, and bind a popup with the Stadiums's name.
+    let stadiumMarker = L.marker((stadCoords),{icon: soccerIcon}).bindPopup(`<h1>${stadium.properties.stadium}</h1><hr><h2>Capcity: ${stadium.properties.cap}</h2><hr><h3>Fact: ${stadium.properties.trivia} </h3>`)
     
-//     // Add the marker to the stadiumMarkers array.
-//     // console.log(stadCoords)
-//     stadiumMarkers.push(stadiumMarker)
-//   }
-//   console.log(stadiumMarkers)
-//   // Create a layer group that's made from the stadium markers array, and pass it to the createMap function.
-//   createMap(L.layerGroup(stadiumMarkers))
-// }
+    // Add the marker to the stadiumMarkers array.
+    // console.log(stadCoords)
+    stadiumMarkers.push(stadiumMarker)
+  }
+  console.log(stadiumMarkers)
+  // Create a layer group that's made from the stadium markers array, and pass it to the createMap function.
+  createMap(L.layerGroup(stadiumMarkers))
+}
 
 
-// // Perform an API call to the Stadmium API to get the station information. Call createMarkers when it completes.
-// // d3.json('/api/stadiums').then(createMarkers(data))
-// d3.json("/api/stadiums").then(data => {
-//   response = data
-//   createMarkers(response)})
+// Perform an API call to the Stadmium API to get the station information. Call createMarkers when it completes.
+// d3.json('/api/stadiums').then(createMarkers(data))
+d3.json("/api/stadiums").then(data => {
+  response = data
+  createMarkers(response)})
 
 // intial code to create sunburst chart using EPL data
 
